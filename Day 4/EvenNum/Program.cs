e22e8aed87fd7cb71e630b4bd13635c45b37fc56
Jithin K Chandran <128501011+JithinKChandran@users.noreply.github.com>
2024-10-04 19:51:32 +0530

    Update EvenNum

--- Day 4/EvenNum/Program.cs
+++ Day 4/EvenNum/Program.cs
@@ -10,15 +10,6 @@ namespace EvenNum
     {
         static void Main(string[] args)
         {
-            /*
-            int[] num = { 1, 2, 3, 4, 5};
-            var firstHalf = num.Take(num.Length/2).Reverse().ToArray();
-            var secondHalf = num.Skip(num.Length/2+1).Reverse().ToArray();
-            firstHalf.CopyTo(num, 0);
-            secondHalf.CopyTo(num, num.Length/2+1);
-            Console.WriteLine(string.Join(" ",num));
-            */
-
             int[] num = { 1, 2, 3, 4};
             var firstHalf = num.Take(num.Length / 2).Reverse().ToArray();
             var secondHalf = num.Skip(num.Length / 2).Reverse().ToArray();
