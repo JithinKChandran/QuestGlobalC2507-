512e547dfc975cd6ae81e254db0deb0a82146516
Jithin K Chandran <128501011+JithinKChandran@users.noreply.github.com>
2024-10-07 17:58:25 +0530

    Day 5 , Online classworks.

---
+++ Day 5/nameSpace/NameSpace/Utilites/StringUtilites.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NameSpace.Utilites
+{
+    internal class StringUtilites
+    {
+        public static void ToupperCase(string text)
+        {
+            Console.WriteLine(text.ToUpper());
+        }
+    }
+}
