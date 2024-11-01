196900cf0a12a7f9acf1e01b3c1657e71308d7d0
Jithin K Chandran <128501011+JithinKChandran@users.noreply.github.com>
2024-11-01 17:51:30 +0530

    Day 23 , Revision day .

---
+++ Day 23/Revision/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Revision
+{
+    class MyClass
+    {
+        public string Name { get; private set; }
+        public void SetValue()
+        {
+            Name = "New Name";
+        }
+    }
+    internal class Program
+    {
+        //static void Add(int i, int j, params int[] array)
+        //{
+        //    Console.WriteLine(array.Sum() + i + j);
+        //}
+        static void Main(string[] args)
+        {
+            //Add(3, 4, 5, 6);
+
+            var n = new MyClass()
+            {
+                // Name = "Test"
+            };
+            
+            Console.WriteLine(n.Name);
+        }
+    }
+}
